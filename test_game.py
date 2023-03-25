ff25eb49a2f28af249d83da632ecec703a9c138b
Cameron Cinel <camcinel@gmail.com>
2023-03-25 12:27:17 -0700

    Got initial variables working

--- test_game.py
+++ test_game.py
@@ -6,13 +6,16 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def main():
     retro.data.Integrations.add_custom_path(os.path.join(SCRIPT_DIR, 'custom_integrations'))
-    env = retro.make('FZero-Snes', state='FZero.KnightCup.Easy.state', inttype=retro.data.Integrations.ALL)
+    print(retro.data.get_file_path('FZero-Snes', 'rom.sha', inttype=retro.data.Integrations.CUSTOM))
+    env = retro.make('FZero-Snes', state='FZero.KnightCup.Easy.state', inttype=retro.data.Integrations.CUSTOM)
     observation = env.reset()
-    for _ in range(10000):
+    for i in range(10000):
         observation, reward, done, info = env.step(env.action_space.sample())
         env.render()
         if done:
             observation = env.reset()
+        if i % 1000 == 0:
+            print(info)
     env.close()
 
 
